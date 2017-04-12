82aa637f8d6d397499d6d4421a5e9376c01daac8
rockyhuber <rocky@rockyhuber.com>
2017-04-12 15:38:25 +1000

    Initial commit

---
+++ test/sample.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Something', () => {
+  describe('The way it smells', () => {
+    it('its pugnent', () => {
+      assert.ok(true);
+    });
+  });
+});
